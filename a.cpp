388c4077f7161be1d62d372037312d3dedaf2609
EsperCS <lnguyenthiennhan@gmail.com>
2026-01-15 14:45:04 +0700

    Add a.cpp

---
+++ a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    if (fopen("input.inp", "r")) {
+        freopen("input.inp", "r", stdin);
+        freopen("output.out", "w", stdout);
+    }
+    cin.tie(0)->sync_with_stdio(0);
+
+    cout << "Hello World";
+
+    return 0;
+}
